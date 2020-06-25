3e148507a8176f1c8150ec960333a1d0ac25af57
Armin Ghofrani <arminghofrani@fastmail.com>
2020-06-25 14:52:37 +0200

    Set up ggez
    Used template from README in ggez/ggez.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+use ggez::event::{self, EventHandler};
+
 fn main() {
-    println!("Hello, world!");
+    // Make a Context.
+    let (mut ctx, mut event_loop) = ContextBuilder::new("collisions-disallowed", "Armin Ghofrani")
+		.build()
+		.expect("Could not create ggez context!");
+
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let mut game = Game::new(&mut ctx);
+
+    // Run!
+    match event::run(&mut ctx, &mut event_loop, &mut game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occured: {}", e)
+    }
+}
+
+struct Game {
+    // Your state here...
+}
+
+impl Game {
+    pub fn new(_ctx: &mut Context) -> Game {
+        // Load/create resources such as images here.
+        Game {
+            // ...
+        }
+    }
+}
+
+impl EventHandler for Game {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Update code here...
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+        // Draw code here...
+        graphics::present(ctx)
+    }
 }
