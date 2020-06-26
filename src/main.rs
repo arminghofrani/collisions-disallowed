476c3eeba8e8385f8849c56c782a4496651d9a1d
Armin Ghofrani <arminghofrani@fastmail.com>
2020-06-26 16:19:52 +0200

    Make circle move
    Also titled window and added FPS counter.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use ggez::{
+    conf::{NumSamples, WindowSetup},
     event::{self, EventHandler},
     *,
 };
 
 fn main() {
+    let window_setup = WindowSetup {
+        title: "collisions-disallowed".to_owned(),
+        samples: NumSamples::Zero,
+        vsync: true,
+        icon: "".to_owned(),
+        srgb: true,
+    };
+
     let (mut ctx, mut event_loop) = ContextBuilder::new("collisions-disallowed", "Armin Ghofrani")
+        .window_setup(window_setup)
         .build()
         .expect("Could not create ggez context!");
 
@@ -16,11 +26,14 @@ fn main() {
     }
 }
 
-struct Game {}
+struct Game {
+    x: f32,
+    y: f32,
+}
 
 impl Game {
     pub fn new(_ctx: &mut Context) -> Game {
-        Game {}
+        Game { x: 0.0, y: 0.0 }
     }
 }
 
@@ -30,18 +43,31 @@ impl EventHandler for Game {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx, graphics::WHITE);
+        let fps = timer::fps(ctx);
+        let fps_display = graphics::Text::new(format!("FPS: {}", fps));
 
         let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
-            mint::Point2 { x: 200.0, y: 200.0 },
+            mint::Point2 {
+                x: self.x,
+                y: self.y,
+            },
             100.0,
             0.1,
             graphics::BLACK,
         )?;
 
+        self.x += 1.0;
+        self.y += 1.0;
+
+        graphics::clear(ctx, graphics::WHITE);
         graphics::draw(ctx, &circle, graphics::DrawParam::default())?;
+        graphics::draw(
+            ctx,
+            &fps_display,
+            (mint::Point2 { x: 0.0, y: 0.0 }, graphics::BLACK),
+        )?;
         graphics::present(ctx)
     }
 }
